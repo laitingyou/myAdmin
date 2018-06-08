b7500bd055658644cf51e5e339122984c0656d2d
laitingyou <1003774479@qq.com>
2018-06-08 18:41:15 +0800

    #init

--- conponents/Hello.js
+++ conponents/Hello.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Button } from 'antd';
 class Hello extends React.Component{
+    click(){
+        console.log(123)
+    }
     render() {
-        return <h1>Hello</h1>;
+        return (
+            <div>
+                <Button onClick={this.click} type="primary">Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="danger">Danger</Button>
+            </div>
+        );
     }
 }
 export default Hello;
